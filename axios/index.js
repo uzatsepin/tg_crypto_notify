a792e3f980aefd4abd7a9b53a0e02719853ab505
Andrii Zatsepin <uzatsepin@gmail.com>
2024-06-22 18:32:15 +0300

    refactoring

--- axios/index.js
+++ axios/index.js
@@ -4,16 +4,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 if (!process.env.API_KEY_COIN_STAT) {
-	throw new Error('API_KEY_COIN_STAT is not defined in the environment variables.');
+    throw new Error("API_KEY_COIN_STAT is not defined in the environment variables.");
 }
 
 const axiosInstance = axios.create({
-	baseURL: 'https://api.coingecko.com/api/v3/',
-	method: 'GET',
-	headers: {
-		accept: 'application/json',
-		'x-cg-demo-api-key': 'CG-44n14TDCKJf1Uu8uZqMK18Ed'
-	}
+    baseURL: process.env.API_URL_COIN_KEGO,
+    method: "GET",
+    headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": process.env.API_COIN_GEKO,
+    },
 });
 
 export default axiosInstance;
